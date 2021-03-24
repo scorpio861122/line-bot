1b265cd7d9f6c7b6370edecdba0128e1ced7c787
thomas <scorpio861122@gmail.com>
2021-03-24 14:55:53 +0800

    update

--- app.py
+++ app.py
@@ -41,6 +41,20 @@ def handle_message(event):
         event.reply_token,
         TextSendMessage(text=event.message.text))
 
+if usage == 'create':
+    rich_menu_to_create = RichMenu(
+        size=RichMenuSize(width=2500, height=843),
+        selected=False,
+        name="Nice richmenu",  # display name
+        chat_bar_text="我是測試使用",
+        areas=[RichMenuArea(  # 這邊是陣列的格式，可以動態設定自己要的區域想要有什麼功能
+            bounds=RichMenuBounds(x=0, y=0, width=2500, height=843),
+            action=URIAction(label='Go to line.me', uri='https://line.me'))]
+    )
+    rich_menu_id = self.line_bot_api.create_rich_menu(
+        rich_menu=rich_menu_to_create)
+    print(rich_menu_id)
+    return {'id': rich_menu_id}, 200
 
 if __name__ == "__main__":
     app.run()
